e211089f54be78e6d103c1ee2e3a5b8d5c3364f2
SaySubr <165163931+SaySubr@users.noreply.github.com>
2024-03-27 20:12:15 +0700

    three two main

--- two.cpp
+++ two.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	
 	std::cout << "Hello world in gitHub"<<std::endl;
-	std::cout << "Hello world in gitHub 2 branch";	
+	std::cout << "Hello world in gitHub 2 branch";
+	std::cout << "gitHub 2 branch is two main";	
 	return 0;
 }
