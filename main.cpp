790712d8ce62b9f5f05da63f4ab9fe6d6a7bc485
SaySubr <165163931+SaySubr@users.noreply.github.com>
2024-04-26 20:30:19 +0700

    51 commit Update

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 int main()
 {
 	std::cout << n << std::endl;
-	std::cout << "Hello world in gitHub";	
+	std::cout << "Hello world in gitHub" << std::endl;
+	#undef N
+	int N = 26;
+	std::cout << N;
 	return 0;
 }
